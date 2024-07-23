db1ad65e86c27f93cb2b9595732490c61501d01b
Calvin Yeboah <113629722+Calvinyeb04@users.noreply.github.com>
2024-07-23 17:53:04 -0400

    feat: Improve landing page layout and styles
    This commit improves the layout and styles of the landing page by adding a background color to the Reasons, Plans, and Programs components. It also adds the HealthWellness component, which displays information about the studio's health and wellness offerings. These changes enhance the visual consistency, functionality, and user experience of the landing page.

--- src/App.js
+++ src/App.js
@@ -1,23 +1,34 @@
 import React from "react";
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import { createBrowserRouter, RouterProvider, Outlet } from "react-router-dom";
 
 import './App.css';
+import Header from './components/Header/Header';
 import Hero from './components/Hero/Hero';
 import Programs from './components/Programs/Programs';
 import Reasons from './components/Reasons/Reasons';
 import Plans from './components/Plans/Plans';
 import GetStarted from './components/GetStarted/GetStarted';
+import HealthWellness from './components/HealthWellness/HealthWellness';
 
 // Import your CSS files here
 import './App.css';
+import './components/Header/Header.css';
 import './components/Hero/Hero.css';
 import './components/Programs/Programs.css';
 import './components/Reasons/Reasons.css';
 import './components/Plans/Plans.css';
 import './components/GetStarted/GetStarted.css';
+import './components/HealthWellness/HealthWellness.css';
 
+// Layout component that includes the Header and Outlet
+const Layout = () => (
+  <>
+    <Header />
+    <Outlet />
+  </>
+);
 
-// New Home component
+// Home component
 const Home = () => (
   <div>
     <Hero />
@@ -30,12 +41,22 @@ const Home = () => (
 const router = createBrowserRouter([
   {
     path: "/",
-    element: <Home />,
-  },
-  {
-    path: "/get-started",
-    element: <GetStarted />,
-  },
+    element: <Layout />,
+    children: [
+      {
+        path: "/",
+        element: <Home />,
+      },
+      {
+        path: "/get-started",
+        element: <GetStarted />,
+      },
+      {
+        path: "/health-wellness",
+        element: <HealthWellness />,
+      }
+    ]
+  }
 ]);
 
 function App() {
