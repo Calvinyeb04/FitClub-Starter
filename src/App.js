f95213bc4c238648f4dd7e9addfbdf2099c79576
Calvin Yeboah <113629722+Calvinyeb04@users.noreply.github.com>
2024-07-24 03:51:32 -0400

    Update the package ,jspn and app.js added the nutrtion to the app js

--- src/App.js
+++ src/App.js
@@ -1,14 +1,16 @@
 import React from "react";
 import { createBrowserRouter, RouterProvider, Outlet } from "react-router-dom";
 
-import './App.css';
 import Header from './components/Header/Header';
 import Hero from './components/Hero/Hero';
 import Programs from './components/Programs/Programs';
 import Reasons from './components/Reasons/Reasons';
 import Plans from './components/Plans/Plans';
-import GetStarted from './components/GetStarted/GetStarted';
 import HealthWellness from './components/HealthWellness/HealthWellness';
+import GetStarted from "./components/GetStarted/GetStarted";
+import WorkoutPlan from "./components/WorkoutPlan/WorkoutPlan";
+import Nutrition from "./components/Nutrition/Nutrition";
+import Menu from "./components/Menu/Menu";
 
 // Import your CSS files here
 import './App.css';
@@ -17,8 +19,13 @@ import './components/Hero/Hero.css';
 import './components/Programs/Programs.css';
 import './components/Reasons/Reasons.css';
 import './components/Plans/Plans.css';
-import './components/GetStarted/GetStarted.css';
 import './components/HealthWellness/HealthWellness.css';
+import './components/GetStarted/GetStarted.css';
+import './components/WorkoutPlan/WorkoutPlan.css';
+import './components/Nutrition/Nutrition.css';
+import './components/Menu/Menu.css';
+
+
 
 // Layout component that includes the Header and Outlet
 const Layout = () => (
@@ -54,6 +61,18 @@ const router = createBrowserRouter([
       {
         path: "/health-wellness",
         element: <HealthWellness />,
+      },
+      {
+        path: "/workout-plan",
+        element: <WorkoutPlan />,
+      },
+      {
+        path: "/Nutrition",
+        element: <Nutrition />,
+      },
+      {
+        path: "/Menu",
+        element: <Menu />,
       }
     ]
   }
